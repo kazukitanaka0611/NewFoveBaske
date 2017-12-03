a48c21752342b6e9561923b6d0ac2a35e6f99df0
zukeyny <zukeyny@gmail.com>
2017-12-03 16:34:44 +0900

    スタート画面を追加

---
+++ Assets/FOVE Sample Scripts/FOVELookSample.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FOVELookSample : MonoBehaviour
+{
+    public Light attachedLight;
+    public FoveInterfaceBase foveInterface;
+
+    private Collider my_collider;
+    private Material material;
+    private bool light_attached = false;
+
+    void Start()
+    {
+        my_collider = GetComponent<Collider>();
+
+        if (attachedLight == null)
+            attachedLight = transform.GetComponentInChildren<Light>();
+
+        if (attachedLight)
+        {
+            light_attached = true;
+            attachedLight.enabled = false;
+        }
+        material = gameObject.GetComponent<Renderer>().material;
+
+        if (material == null)
+            gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (foveInterface.Gazecast(my_collider))
+        {
+            material.EnableKeyword("_EMISSION");
+
+            if (light_attached)
+            {
+                material.SetColor("_EmissionColor", attachedLight.color);
+                attachedLight.enabled = true;
+                SceneManager.LoadScene(1);
+            }
+        }
+        else
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.white;
+            material.DisableKeyword("_EMISSION");
+
+            if (light_attached)
+            {
+                attachedLight.enabled = false;
+                DynamicGI.SetEmissive(GetComponent<Renderer>(), Color.black);
+            }
+        }
+    }
+}
