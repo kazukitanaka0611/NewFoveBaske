26eba376237b82b67dcaa4f0eeb833cb63114e38
DESKTOP-M4OLME2\mynam <name4yu@gmail.com>
2017-12-09 15:49:55 +0900

    ゴール文字表示

--- Assets/Scripts/GoalBottom.cs
+++ Assets/Scripts/GoalBottom.cs
@@ -10,6 +10,8 @@ public class GoalBottom : MonoBehaviour
 
     public AudioClip se;
 
+    public GameObject goalText;
+
     public void SetCollider(Collider collider)
     {
         parentCollider = collider;
@@ -24,10 +26,27 @@ public class GoalBottom : MonoBehaviour
             if (scoreManager)
             {
                 GetComponent<AudioSource>().PlayOneShot(se);
+                StartCoroutine("GoalDsp");
                 scoreManager.AddScore(2);
             }
-           
+
         }
+
+    }
+
+    // ゴール文字表示関数
+    private IEnumerator GoalDsp()
+    {
+        Debug.Log("1");
+        // ゴール文字表示
+        goalText.SetActive(true);
+
+        // 0.5秒待つ  
+        yield return new WaitForSeconds(0.5f);
+
+        // ゴール文字非表示
+        goalText.SetActive(false);
+
 
     }
 }
